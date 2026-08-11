d5cf66c9a2cd569b60d6b0398d84db7d8310ca13
Abhishek Singh <sabhishek900@gmail.com>
2026-08-11 14:55:31 +0000

    Frontend file structure completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Audiobook Reader</h1>
+
+      <p>
+        Listen to your audiobook while reading the corresponding book.
+      </p>
+
+      <button>Open Book</button>
+    </div>
+  );
+}
+
+export default App;
